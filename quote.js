d272f0297403f88b39ed47ae5e7e99f8844da35d
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-07-25 13:48:43 +0100

    interim commit

--- quote.js
+++ quote.js
@@ -1,36 +1,21 @@
 import fs from "node:fs/promises";
-// import { readFile } from 'node:fs/promises';
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "quotes.json";
 
-async function readFromFile(filePath) {
-    try {
-        const data = await fs(filePath, 'utf8');
-        console.log(data);
-    } catch (error) {
-        console.error('Error reading file:', error);
-    }
-}
-
 export async function getQuotes() {
-    readFromFile(fileName);   
+  try {
+    const data = await fs(fileName, "utf8");
+    console.log(data);
+  } catch (error) {
+    console.error("Error reading file:", error);
+  }
 }
 
-export async function addQuote(quoteText) {
+export async function addQuote(quoteText) {}
 
-}
+export async function getRandomQuote() {}
 
-export async function getRandomQuote() {
+export async function editQuote(id, quoteText) {}
 
-}
-
-export async function editQuote(id, quoteText) {
-
-}
-
-export async function deleteQuote(id) {
-
-}
-
-getQuotes();
+export async function deleteQuote(id) {}
